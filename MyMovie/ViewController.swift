368f8f5170de99162298f0f57b777fdcf86011b1
Rodnei A. Ribeiro <rodnei.solucoes@gmail.com>
2023-03-18 13:10:52 -0300

    Initial Commit

---
+++ MyMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovie
+//
+//  Created by Rodnei Albuquerque on 18/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
